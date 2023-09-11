8444b2b069d1c7c17b8edbe543e74e4a19cc7a85
me <me@naver.com>
2023-09-11 09:29:49 +0900

    다리를 지나는 트럭

--- test4.js
+++ test4.js
@@ -1,6 +1,13 @@
-console.log(
-  [2, 3, 4, 5, 6].reduce((a, b) => {
-    console.log(a, b);
-    return a + b;
-  })
-);
+function solution(bridge_length, weight, truck_weights) {
+  let time = 0;
+  let bridge = Array(bridge_length).fill(0);
+  let weight = 0;
+  while (truck_weights) {
+    weight = weight - bridge.shift();
+    if (bridge.length) {
+    }
+  }
+  return time;
+}
+
+solution(2, 10, [7, 4, 5, 6]);
--- 다리를 지나는 트럭.js
+++ 다리를 지나는 트럭.js
@@ -2,12 +2,12 @@ function solution(bridge_length, weight, truck_weights) {
   let time = 0;
   let bridge = Array(bridge_length).fill(0);
 
-  while (truck_weights.length) {
+  while (bridge.length) {
     bridge.shift();
     if (truck_weights.length) {
       //지나갈 트럭이존재하면
       const weightSum = bridge.reduce((acc, v) => acc + v, 0);
-      console.log(weightSum);
+      // console.log(weightSum);
       if (weightSum + truck_weights[0] <= weight) {
         //트럭의 무게가 다리를 지나갈 수 있으면
         bridge.push(truck_weights.shift());
@@ -15,7 +15,7 @@ function solution(bridge_length, weight, truck_weights) {
         bridge.push(0);
       }
     }
-
+    console.log(time);
     time++;
   }
   return time;
